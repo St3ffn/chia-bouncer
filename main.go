a1b2409da028a9138fabf7a20e42c22f34f7c462
st3ffn <funk.up.up@gmail.com>
2021-05-16 13:15:20 +0200

    add main

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
-	"chia-bouncer/bouncer"
 	"fmt"
+	"github.com/St3ffn/chia-bouncer/bouncer"
 	"os"
 )
 
 func main() {
 	if err := run(); err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+		_, _ = fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
 }
 
+// run will initialise the cli, get all connected full nodes, filter them by given location and
+// finally remove filtered full nodes
 func run() error {
-	_, err := bouncer.Run()
+	ctx, err := bouncer.RunCli()
 	if err != nil {
 		return err
 	}
+	if ctx.Done {
+		return nil
+	}
+	chiaCli := bouncer.ChiaCli{ChiaExecutable: ctx.ChiaExecutable}
+	nodes, err := chiaCli.ListNodes()
+	if err != nil {
+		return err
+	}
+	filter := bouncer.FilterByLocation{
+		Nodes:            nodes,
+		LocationToFilter: ctx.Location,
+	}
+	filtered, err := filter.Filter()
+	if err != nil {
+		return err
+	}
+
+	if len(filtered) == 0 {
+		_, _ = fmt.Fprintf(os.Stdout, "nothing from %s\n", ctx.Location)
+	}
+
+	for _, node := range filtered {
+		if err := chiaCli.RemoveNode(node.NodeId); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Can not filter %s:%s from %s...stopping\n",
+				node.Ip, node.NodeId, ctx.Location)
+			return err
+		}
+	}
+	_, _ = fmt.Fprintf(os.Stdout, "found %d - filtered %d from %s\n", len(nodes), len(filtered), ctx.Location)
 	return nil
 }
